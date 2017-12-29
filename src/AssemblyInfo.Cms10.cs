24bb886992ba32b58c9dc734999bc5437e3ccad9
Vladimir Levchuk <vladimir.levchuk@creuna.no>
2017-12-29 11:11:03 +0200

    fixed build-packages.cmd, nuget packages are now built using csproj files

---
+++ src/AssemblyInfo.Cms10.cs
@@ -0,0 +1,3 @@
+using System.Reflection;
+[assembly: AssemblyVersion("10.2.1.0")]
+[assembly: AssemblyInformationalVersion("10.2.1")]
---
+++ src/AssemblyInfo.Cms11.cs
@@ -0,0 +1,3 @@
+using System.Reflection;
+[assembly: AssemblyVersion("11.0.1.0")]
+[assembly: AssemblyInformationalVersion("11.0.1")]
